4ce844ec9705948a124bdc122ddee6652dde4c61
Felipe Macedo <feliped@gmail.com>
2013-07-07 20:41:29 +0000

    hopefully reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  Buffer indexHtml = fs.readFileSync('index.html');
+  response.send(indexHtml.toString());
 });
 
 var port = process.env.PORT || 5000;
